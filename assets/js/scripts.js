590d0facd27189f1ceeca0e86fdd920579b1c26f
Fomcredo <focremdo@ingenieria.unam.edu>
2024-03-02 09:43:38 -0600

    Cambios aplicados a secciones
    Cambios en Colaboradores, Actualización de videos en Productos, Cambios en Eventos

---
+++ assets/js/scripts.js
@@ -0,0 +1,60 @@
+/*//SCRIPT PARA SIDENAV, COLLAPSIBLE Y CAROUSEL
+
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+//Inicializacion del Collapsible
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+//Inicializacion del Carousel con opciones personalizadas
+
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+      fullWidth: true, // Carousel ocupa todo el ancho de su contenedor
+      indicators: true,
+      dist:0, // Muestra los indicadores de posición
+      duration: 200, // Duración de la transición en milisegundos
+      dist: -800, // Distancia entre los elementos (negativa para mostrar más elementos)
+      //shift: 50, // Desplazamiento al usar los indicadores
+      numVisible: 1,
+    };
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+
+    function nextSlide(){
+      var instance = M.Carousel.getInstance(elems[0]);
+      instance.next();
+    }
+    setInterval(nextSlide, 3500);
+
+});*/
+$(document).ready(function(){
+    //Inicializacion del Sidenav
+    $('.sidenav').sidenav();
+
+    //Inicializacion del Collapsible
+    $('.collapsible').collapsible();
+    
+    //Inicializacion del Carousel con opciones personalizadas
+    var options = {
+      fullWidth: true,
+      indicators: true,
+      //dist: -800,
+      //duration: 3500,
+      //numVisible: 1,
+    };
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  
+    function nextSlide(){
+      var instance = M.Carousel.getInstance(elems[0]);
+      instance.next();
+    }
+    setInterval(nextSlide, 3500);
+  });
